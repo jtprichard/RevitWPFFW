c9a6ff052533fecbf2dd7e0962dee59940dec5a3
Jason Prichard <jprichard@performancebim.com>
2021-09-25 09:14:50 -0400

    missed items in commit

---
+++ RevitWPFFW.core/RevitDocument - ORIGINAL.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.DB;
+
+namespace RevitWPFFW.core
+{
+    /// <summary>
+    /// Keeps a copy of the current Revit Document
+    /// </summary>
+    public class RevitDocument
+    {
+        private static Document _document;
+
+        public RevitDocument(Document doc)
+        {
+            _document = doc;
+        }
+
+        /// <summary>
+        /// Returns the current Revit document
+        /// </summary>
+        /// <returns></returns>
+        public static Document GetCurrentDocument()
+        {
+            return _document;
+        }
+    }
+}
---
+++ RevitWPFFW.core/ViewModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitWPFFW.core
+{
+    public class ViewModels
+    {
+        private static ViewModels _currentViewModel;
+
+        public static ViewModels CurrentViewModels => _currentViewModel;
+
+        public MainPageViewModel MainViewModel { get; private set; }
+        public Page1BViewModel Page1BViewModel { get; private set; }
+        public Page1ViewModel Page1ViewModel { get; private set; }
+        public Page2ViewModel Page2ViewModel { get; private set; }
+
+        public ViewModels()
+        {
+            MainViewModel = new MainPageViewModel();
+            Page1ViewModel = new Page1ViewModel();
+            Page1BViewModel = new Page1BViewModel();
+            Page2ViewModel = new Page2ViewModel();
+        }
+
+    }
+}
